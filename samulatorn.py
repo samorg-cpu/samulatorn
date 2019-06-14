998a0a14a3411b4ef1400dbdd69ef7bc15c73821
samorg-cpu <51823283+samorg-cpu@users.noreply.github.com>
2019-06-14 20:23:28 +0800

    Create samulatorn.py

---
+++ samulatorn.py
@@ -0,0 +1,24 @@
+print("Welcome to Samulator\n version 1.1 Al2O3 ")
+print("/***\ /***\ |\  /|   |           TM ");
+print("|___  |___| | \/ |   |   _|_      ");
+print("    | |   | |    || ||/*\ | /*\|/ ");
+print("\___/ |   | |    |\_/|\_/\| \_/|  ");
+print("This project follows the GPL v3 lisence.C and Java edition also exist. Copyright® Sam.org\n");
+print("issues and pull requests please go to NeXT286@Github or samorg-cpu. Don't be  modest!\n\n\n\n");
+print("By SAM.org, bug report hotline:hansonchen600@gmail.com")
+choice1=str(input("Do you want to see our update log?    "))
+if choice1=="yes":
+    print("0.1   Ac  Basic calculation")
+def sim():
+    H=str(input("the calculation（add the space）："))
+    He=str.split()
+    if He[1]=="+":
+        Li=He[0]+He[1]
+    if He[1]=="-":
+        Li=He[0]-He[1]
+    if He[1]=="*":
+        Li=He[0]*He[1]
+    if He[1]=="/":
+        Li=He[0]/He[1]
+    if He[1]=="**" or He[1]=="^":
+        Li=He[0]**He[1]
